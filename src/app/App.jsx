63d7493049bdb6001a9fb9eff989eead53484c08
DylanPelle <dylan.pelle.dev@gmail.Com>
2022-09-15 14:19:04 +0200

    Add and init redux-persist, Next step, see if it works

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "../utils/store";
-
+import { persistor, store } from "../utils/store";
+import { PersistGate } from "redux-persist/integration/react";
 import RoutesApp from "./Routes.jsx";
+import Loader from "./Loader.jsx";
+
 const App = () => {
 	return (
 		<Provider store={store}>
-			<BrowserRouter>
-				<RoutesApp />
-			</BrowserRouter>
+			<PersistGate loading={<Loader />} persistor={persistor}>
+				<BrowserRouter>
+					<RoutesApp />
+				</BrowserRouter>
+			</PersistGate>
 		</Provider>
 	);
 };
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,8 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
-import employeeReducer from "../features/employee"
+import { configureStore } from "@reduxjs/toolkit";
 
-export default configureStore({
-    reducer: {
-        employee: employeeReducer,
-    },
-})
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+
+import employeeReducer from "../features/employee";
+const persistConfig = {
+	key: "root",
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, employeeReducer);
+
+export const store = configureStore({
+	reducer: {
+		employee: persistedReducer,
+	},
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
+});
+export const persistor = persistStore(store)
